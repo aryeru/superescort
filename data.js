049a9fe0f855efd714a4ce2fb9212003b0a384cd
arielsolo <aryeru@yahoo.com.ar>
2026-07-22 11:34:51 -0300

    Respaldo inicial antes de fusionar

---
+++ data.js
@@ -0,0 +1,18 @@
+const professionals = [
+{
+  "name": "Scarlet",
+  "phone": "+34 600 123 456",
+  "services": "Heroína, Fuerza y poder",
+  "zone": "USA",
+  "photo": "images/scarlet_cara.jpg",
+  "review": "Super heroína de Marvel, ha salvado muchas vidas.",
+},
+{
+  "name": "Super Girl",
+  "phone": "+34 600 654 321",
+  "services": "Heroína, Periodista",
+  "zone": "USA",
+  "photo": "images/supergirl1_cara.jpg",
+  "review": "Super heroína de DC, ha salvado muchas vidas."
+} // Puedes agregar más profesionales aquí
+];
